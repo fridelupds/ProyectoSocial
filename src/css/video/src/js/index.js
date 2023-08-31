db8740d8d662ea8bcbd0c439245aa125ce9cb7f8
Rocío Cruz Zambrana <141836344+RocioCruz2004@users.noreply.github.com>
2023-08-31 18:02:24 -0400

    Delete src directory

--- src/css/video/src/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const video = document.getElementById("vVideo");
-  video.addEventListener("ended", () => {
-    document.body.classList.add("transicionJS");
-    setTimeout(() => {
-      window.location.href = "otro.html";
-    }, 1000); 
-  });
-});
